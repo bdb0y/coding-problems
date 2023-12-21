600ff7f3335ba979bc5f5dd1eaeb85bbdce89908
amin <bandar3eda021@gmail.com>
2023-12-21 16:29:50 +0330

    r-x-m-problem

---
+++ regular-expression-matching.py
@@ -0,0 +1,15 @@
+import re
+
+def isMatch(s: str, p: str) -> bool:
+        if '**' in p:
+            i = p.index('*')
+            p = p.replace('*', '')
+            p = p[:i] + '*' + p[i:]
+        if re.search('^{0}$'.format(p), s):
+            return True
+        return False
+    
+    
+s, p = input().split()
+
+print(str(isMatch(s, p)).lower())
