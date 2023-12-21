addcfaa3992c77ad551247efcc1ee53cd586d667
amin <bandar3eda021@gmail.com>
2023-12-21 16:39:54 +0330

    new problem

---
+++ partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,9 @@
+
+# url: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+
+def minPartitions(n: str) -> int:
+        
+        return int(max(n))
+    
+    
+print(minPartitions(input()))
